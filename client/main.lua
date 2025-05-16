af577d65d7d2d5db1abf4435a0493c6be8169d23
Khazadream <97912361+Khazadream@users.noreply.github.com>
2025-05-16 17:12:45 +0200

    Fix metadata propName

--- client/main.lua
+++ client/main.lua
@@ -15,18 +15,19 @@ RegisterNetEvent('ka-placeableitems:client:placePlaceItem', function(obj, metada
     local distance = 100 -- distance to spawn object
     local type = 'none' -- object type (none or container)
     local name = QBCore.Functions.GetPlayerData().charinfo.firstname .. ' ' .. QBCore.Functions.GetPlayerData().charinfo.lastname -- player name
-    local object = obj
+    local object = metadata.propName
     
     placeItemOnGround(distance, type, name, object, metadata)
 end)
 
 exports('testplaceable', function(data, slot)
     if not slot.metadata then return end
-    
-    local object = slot.metadata.type
+
+    local object = slot.metadata.propName
     local distance = 100 -- distance to spawn object
     local type = 'none' -- object type (none or container)
     local name = QBCore.Functions.GetPlayerData().charinfo.firstname .. ' ' .. QBCore.Functions.GetPlayerData().charinfo.lastname -- player name
     local metadata = slot.metadata
+    
     placeItemOnGround(distance, type, name, object, metadata)
 end)
